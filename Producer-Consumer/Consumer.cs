d6d11057585e4261bebae672b4428779777c9375
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-03-21 19:22:21 -0600

    Consumer & Producer class

---
+++ Producer-Consumer/Consumer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer_Consumer
+{
+    class Consumer
+    {
+        private int state { get; set; }
+        private int currentPos { get; set; }
+
+        public Consumer()
+        {
+            state = 0;
+            currentPos = 0;
+        }
+        public bool Consume()
+        {
+            return false;
+        }
+
+    }
+}
---
+++ Producer-Consumer/Producer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer_Consumer
+{
+    class Producer
+    {
+        private int state { get; set; }
+        private int currentPos { get; set; }
+
+        public Producer()
+        {
+            state = 0;
+            currentPos = 0;
+        }
+
+        public bool Produce()
+        {
+            return true;
+        }
+    }
+}
